2ce9d03d4e6f60bf0fcc1eb944b7ae94d8db1901
olfaG <109790045+olfaG@users.noreply.github.com>
2022-10-18 14:55:17 +0200

    Update p_principale.php

--- p_principale.php
+++ p_principale.php
@@ -189,9 +189,9 @@ Aucun résultat pour: <?= $q ?>...
         </thead>
         <tbody>
 
-            <!-- AFFICHER LA TABLE EMPRUNT -->
+            <!-- AFFICHER LA TABLE STAGIAIRES -->
             <?php 
-            include('show_data_emprunt.php');
+            include('show_stag.php');
             ?>
 
             <!-- ------------POUR L'IMPORT CSV-------------- -->
@@ -207,7 +207,6 @@ Aucun résultat pour: <?= $q ?>...
 
   <!-- -------------END DATATABLE------------------- -->
   
-<script src="../../ressources/js/datatable_set.js"></script>
   <!-- JS SCRIPT -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
